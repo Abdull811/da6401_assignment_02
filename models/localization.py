7e54255bf7cf6c4ca161d20d41b10023c3145370
Abdull811 <ge26z811@smail.iitm.ac.in>
2026-04-05 17:08:25 +0300

    Update localization.py

--- models/localization.py
+++ models/localization.py
@@ -7,13 +7,15 @@ import torch
 import numpy as np
 import torch.nn as nn
 
+
 np.random.seed(42)
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
 
-from models.vgg11 import VGG11
+from models.layers import CustomDropout
+from models.vgg11 import VGG11Encoder
 
 
 class VGG11Localizer(nn.Module):
@@ -30,14 +32,19 @@ class VGG11Localizer(nn.Module):
         
         super().__init__()
         # Extract features from image
-        self.encoder = VGG11()
-        
-        # Convert features into bounding box coordinates
+        self.image_size = 224.0 # Fixed image size
+        self.encoder = VGG11Encoder(in_channels=in_channels) # Encoder
         self.head = nn.Sequential(
-        nn.Flatten(),
-        nn.Linear(512 * 7 * 7, 1024),
-        nn.ReLU(),
-        nn.Linear(1024, 4))
+            nn.Flatten(),
+
+            nn.Linear(512 * 7 * 7, 2048),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(2048, 512),
+            nn.ReLU(inplace=True),
+            
+            nn.Linear(512, 4),
+        )
         
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -48,11 +55,15 @@ class VGG11Localizer(nn.Module):
         Returns:
             Bounding box coordinates [B, 4] in (x_center, y_center, width, height) format in original image pixel space(not normalized values).
         """
+        
         x = self.encoder(x)
-        x = self.head(x)
+        raw_box = self.head(x)
 
-        centers = torch.sigmoid(x[:, :2]) * 224.0
-        sizes = torch.sigmoid(x[:, 2:]) * 224.0
+        # Normalize outputs
+        centers = torch.sigmoid(raw_box[:, :2]) * self.image_size
+        sizes = torch.sigmoid(raw_box[:, 2:]) * self.image_size
+
+        # Avoid zero-size boxes
+        sizes = torch.clamp(sizes, min=1.0)
 
         return torch.cat([centers, sizes], dim=1)
-    
