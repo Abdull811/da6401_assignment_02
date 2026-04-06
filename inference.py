b9c6a6f78c5bf682080b6bb7910a6ce946c47b90
Abdull811 <ge26z811@smail.iitm.ac.in>
2026-04-06 22:28:43 +0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -20,20 +20,56 @@ wandb.init(project="da6401_Assigment_02_Weight_&_Biase")
 model = MultiTaskPerceptionModel()
 model.eval()
 
-# Load external image
+def colorize_mask(mask):
+    colored = np.zeros((mask.shape[0], mask.shape[1], 3), dtype=np.uint8)
+
+    colored[mask == 0] = [0, 0, 0]
+    colored[mask == 1] = [0, 255, 0]
+    colored[mask == 2] = [255, 0, 0]
+
+    return colored
+
+# Load image
 img = mpimg.imread("data/images/Abyssinian_1.jpg").astype(np.float32)
 
 if img.max() > 1:
     img = img / 255.0
 
-img = torch.tensor(img).permute(2,0,1).unsqueeze(0)
+img_tensor = torch.tensor(img).permute(2,0,1).unsqueeze(0)
 
+# Forward pass
 with torch.no_grad():
-    out = model(img)
+    out = model(img_tensor)
 
-pred_mask = torch.argmax(out["segmentation"][0], dim=0)
+# Outputs
+pred_mask = torch.argmax(out["segmentation"][0], dim=0).cpu().numpy()
+pred_box = out["localization"][0].cpu().numpy()
 
+# Convert image for visualization
+img_np = (img * 255).astype(np.uint8)
+
+# Draw bounding box (no cv2)
+def draw_box(img, box, color):
+    img = img.copy()
+    xc, yc, w, h = box
+
+    x1 = int(xc - w/2)
+    y1 = int(yc - h/2)
+    x2 = int(xc + w/2)
+    y2 = int(yc + h/2)
+
+    img[y1:y1+2, x1:x2] = color
+    img[y2-2:y2, x1:x2] = color
+    img[y1:y2, x1:x1+2] = color
+    img[y1:y2, x2-2:x2] = color
+
+    return img
+
+img_box = draw_box(img_np, pred_box, [255,0,0])
+
+# Log to W&B
 wandb.log({
-    "final_input": wandb.Image(img[0].permute(1,2,0).numpy()),
-    "final_pred_mask": wandb.Image(pred_mask.numpy() * 100)
+    "final_pipeline_image": wandb.Image(img_np),
+    "final_pipeline_mask": wandb.Image(colorize_mask(pred_mask)),
+    "final_pipeline_bbox": wandb.Image(img_box)
 })
