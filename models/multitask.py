7bad136040d7e57767e3e6766e25bc06b7655279
Abdull811 <ge26z811@smail.iitm.ac.in>
2026-04-07 13:45:27 +0300

    Remove unused import for VGG11Encoder
    Removed import statement for VGG11Encoder from multitask.py.

--- models/multitask.py
+++ models/multitask.py
@@ -8,7 +8,6 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if PROJECT_ROOT not in sys.path:
      sys.path.insert(0, PROJECT_ROOT)
 
-from models.vgg11 import VGG11Encoder
 from models.classification import VGG11Classifier
 from models.localization import VGG11Localizer
 from models.segmentation import VGG11UNet
