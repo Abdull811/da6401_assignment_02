e47424d956582ad3f9c9e8e5e14a1ae6c8ab3b3c
Abdull811 <ge26z811@smail.iitm.ac.in>
2026-04-07 11:45:20 +0300

    Disable downloading of model weights
    Comment out gdown download calls for weights.

--- models/multitask.py
+++ models/multitask.py
@@ -30,10 +30,10 @@ class MultiTaskPerceptionModel(nn.Module):
             unet_path: Path to trained unet weights.
         """
         super().__init__()
-        import gdown
-        gdown.download(id="1fYI-OkSUIeIgaHHnnd71a17UHD7Z_J9w", output=classifier_path, quiet=False)
-        gdown.download(id="1ze_1tP6JJhBq1F_zTOSHrsiIlz_13-pb", output=localizer_path, quiet=False)
-        gdown.download(id="1RfaqmEep6YkevPyA-2-ZwOTNnVlyOeub", output=unet_path, quiet=False)
+        #import gdown
+        #gdown.download(id="1fYI-OkSUIeIgaHHnnd71a17UHD7Z_J9w", output=classifier_path, quiet=False)
+        #gdown.download(id="1ze_1tP6JJhBq1F_zTOSHrsiIlz_13-pb", output=localizer_path, quiet=False)
+        #gdown.download(id="1RfaqmEep6YkevPyA-2-ZwOTNnVlyOeub", output=unet_path, quiet=False)
 
         # Shared Backbone
         self.encoder = VGG11Encoder(in_channels)
