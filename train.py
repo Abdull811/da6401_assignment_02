afebc7b10f0adceb9fd8f001c5db9b78d2c03a7a
Abdull811 <ge26z811@smail.iitm.ac.in>
2026-04-07 16:07:06 +0300

    Add WANDB_API_KEY to environment variables
    Set the WANDB_API_KEY environment variable for WandB integration.

--- train.py
+++ train.py
@@ -16,6 +16,9 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
 
+os.environ["WANDB_API_KEY"] = "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"
+
+
 from data.pets_dataset import OxfordIIITPetDataset
 from losses.iou_loss import IoULoss
 from models.classification import VGG11Classifier
